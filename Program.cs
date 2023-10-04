e55e4c0bb9328063e3d7f6b897416d0b88ca9151
MrSEKIRO <ab.arshia@gmail.com>
2023-10-04 22:01:09 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeGenerationWithRoslyn
+{
+	partial class Program
+	{
+		static void Main(string[] args)
+		{
+			HelloFrom("Compiler Class");
+		}
+
+		static partial void HelloFrom(string name);
+	}
+}
